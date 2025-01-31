8830b60967b586906500e5133c15dfb7f40842fa
5harath <shartja@gmail.com>
2025-01-31 18:00:11 +0530

    9th commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Navbar } from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Shop } from './Pages/Shop';
 import { ShopCategory } from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup'
 
 function App() {
   return (
@@ -14,6 +17,11 @@ function App() {
           <Route path='/mens' element={<ShopCategory/>}/>
           <Route path='/kids' element={<ShopCategory/>}/>
           <Route path='/womens' element={<ShopCategory/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Loginsignup = () => {
+export const LoginSignup = () => {
   return (
     <div>Loginsignup</div>
   )
