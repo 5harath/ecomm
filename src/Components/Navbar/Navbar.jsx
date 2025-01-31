97f4f31d0fecf5823436db62f84d60e8e1443995
Sharath J <shartja@gmail.com>
2025-01-31 14:00:19 +0530

    6th commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 export const Navbar = () => {
   return (
-    <div className='Navbar'>
-        
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt="" />
+            <p>CHOPPER</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop <hr/></li>
+            <li>Men <hr/></li>
+            <li>Kids <hr/></li>
+            <li>Women <hr/></li>
+        </ul>
+        <div className='nav-login-cart'>
+            <button>Login</button>
+            <img src={cart_icon} alt="" />
+            <div className="nav-cart-count">0</div>
+        </div>
     </div>
   )
 }
