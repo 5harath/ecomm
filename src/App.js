2aa9a40bc71e233e07e6920e9feff912172e8de4
5harath <shartja@gmail.com>
2025-02-04 04:35:51 +0000

    add sandbox

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import './App.css';
-import { Navbar } from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Shop } from './Pages/Shop';
-import { ShopCategory } from './Pages/ShopCategory';
-import { Product } from './Pages/Product';
-import { Cart } from './Pages/Cart';
-import { LoginSignup } from './Pages/LoginSignup'
+import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Shop } from "./Pages/Shop";
+import { ShopCategory } from "./Pages/ShopCategory";
+import { Product } from "./Pages/Product";
+import { Cart } from "./Pages/Cart";
+import { LoginSignup } from "./Pages/LoginSignup";
+import { Footer } from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory/>}/>
-          <Route path='/kids' element={<ShopCategory/>}/>
-          <Route path='/womens' element={<ShopCategory/>}/>
-          <Route path='/product' element={<Product/>}>
-            <Route path=':productId' element={<Product/>}/>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory />} />
+          <Route path="/kids" element={<ShopCategory />} />
+          <Route path="/womens" element={<ShopCategory />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/login' element={<LoginSignup/>}/>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
